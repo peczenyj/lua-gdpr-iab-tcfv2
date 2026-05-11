7083c225f68cff98f60f159fd29a6168bdd2bb6b
Tiago Peczenyj <tpeczenyj@weborama.com>
2026-05-11 02:12:16 +0200

    feat(test): implement global fuzz data optimization and update script

--- test/reference/golden_spec.lua
+++ test/reference/golden_spec.lua
@@ -10,7 +10,7 @@ describe("Golden Corpus Integrity", function()
 
     local md5 = result:match("^(%x+)")
     assert.are.equal(
-      "ad263e65897df0064d3e27e792e6e3f6",
+      "09aef9de9e6fb6ecbab08683dd87336c",
       md5,
       "Golden corpus file is corrupted or modified"
     )
